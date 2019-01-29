acd6903323ff85435d8e6ebefed57a46cd064c1e
Smit Shah <Smit Shah@Smit>
2019-01-29 18:43:53 +0530

    RemoveDuplicatesFromSortedList

--- epi/RemoveDuplicatesFromSortedList.java
+++ epi/RemoveDuplicatesFromSortedList.java
@@ -5,8 +5,24 @@ public class RemoveDuplicatesFromSortedList {
   @EpiTest(testDataFile = "remove_duplicates_from_sorted_list.tsv")
 
   public static ListNode<Integer> removeDuplicates(ListNode<Integer> L) {
-    // TODO - you fill in here.
-    return null;
+    
+	if (L == null) {
+		return null;
+	}
+	ListNode<Integer> iter = L;
+	int temp = iter.data;
+	while(iter != null && iter.next != null) {
+		
+		if(temp != iter.next.data) {
+			iter = iter.next;
+			temp = iter.data;
+		}
+		else {
+			iter.next = iter.next.next;
+		}
+		
+	}
+    return L;
   }
 
   public static void main(String[] args) {
