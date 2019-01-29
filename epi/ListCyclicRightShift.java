2326e0141bda2baae25f2432ac8b669c443958b5
Smit Shah <Smit Shah@Smit>
2019-01-29 19:36:50 +0530

    ListCyclicRightShift

--- epi/ListCyclicRightShift.java
+++ epi/ListCyclicRightShift.java
@@ -6,8 +6,47 @@ public class ListCyclicRightShift {
 
   public static ListNode<Integer> cyclicallyRightShiftList(ListNode<Integer> L,
                                                            int k) {
-    // TODO - you fill in here.
-    return null;
+	  
+	  if(L == null) {
+		  return null;
+	  }
+	  int length = -1;
+	  ListNode<Integer> head = new ListNode<Integer>(0, L);
+	  ListNode<Integer> iter = head;
+	  ListNode<Integer> slow = L;
+	  ListNode<Integer> fast = L;
+	  while(iter != null) {
+		  iter = iter.next;
+		  length++;
+	  }
+	  iter = head;
+	  
+	  if(k>=length) {
+		  k = k % length;
+	  }
+	  
+	  if(k == 0) {
+		  return L;
+	  }
+	  
+	  while(k-- > 0) {
+		  fast = fast.next;
+	  }
+	  
+	  while(fast.next != null) {
+		  slow = slow.next;
+		  fast = fast.next;
+	  }
+	  
+	  ListNode<Integer> temp = slow.next;
+	  slow.next = null;
+	  iter.next = temp;
+	  while(iter.next != null) {
+		  iter = iter.next;
+	  }
+	  iter.next = L;
+	  
+	  return head.next;
   }
 
   public static void main(String[] args) {
