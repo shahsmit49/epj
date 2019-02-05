0fd9e3007e0541761a980a4d6d0831483851b5b4
Smit Shah <Smit Shah@Smit>
2019-02-05 19:30:16 +0530

    LowestCommonAncestor

--- epi/LowestCommonAncestor.java
+++ epi/LowestCommonAncestor.java
@@ -5,12 +5,51 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TestFailure;
 import epi.test_framework.TimedExecutor;
 public class LowestCommonAncestor {
-  public static BinaryTreeNode<Integer> LCA(BinaryTreeNode<Integer> tree,
-                                            BinaryTreeNode<Integer> node0,
+	
+	
+	
+	private static class Status {
+		
+		int numOfTargets;
+		BinaryTreeNode<Integer> ancestor;
+		public Status(int numOfTargets, BinaryTreeNode<Integer> ancestor) {
+			super();
+			this.numOfTargets = numOfTargets;
+			this.ancestor = ancestor;
+		}
+	}
+	
+	public static BinaryTreeNode<Integer> LCA(BinaryTreeNode<Integer> tree,
+											BinaryTreeNode<Integer> node0,
                                             BinaryTreeNode<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
-  }
+    
+	
+		return recursiveTree(tree, node0, node1).ancestor;
+	}
+	
+	public static Status recursiveTree(BinaryTreeNode<Integer> tree,
+										BinaryTreeNode<Integer> node0,
+										BinaryTreeNode<Integer> node1) {
+		
+		if(tree == null) return new Status(0, null);
+		
+		Status statusOfLeft = recursiveTree(tree.left, node0, node1);
+		if(statusOfLeft.numOfTargets == 2) {
+			return statusOfLeft;
+		}
+		
+		Status statusOfRight = recursiveTree(tree.right, node0, node1);
+		if(statusOfRight.numOfTargets == 2) {
+			return statusOfRight;
+		}
+		
+		int numOftargets = statusOfLeft.numOfTargets + statusOfRight.numOfTargets + (tree == node0 ? 1 : 0) 
+																					+ (tree == node1 ? 1 : 0);
+		
+		return new Status(numOftargets, numOftargets == 2 ? tree : null);
+		
+	}
+  
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor,
                                BinaryTreeNode<Integer> tree, Integer key0,
