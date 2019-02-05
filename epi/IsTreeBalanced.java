c4335dc9a2ee81fef88042dc9031b64fee93b8e0
Smit Shah <Smit Shah@Smit>
2019-02-05 14:07:40 +0530

    IsTreeBalanced

--- epi/IsTreeBalanced.java
+++ epi/IsTreeBalanced.java
@@ -6,8 +6,35 @@ public class IsTreeBalanced {
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    
+	  int a = treeTraversal(tree);
+	  if(a < 0 )
+		  return false;
+	  return true;
+  }
+  
+  public static int treeTraversal(BinaryTreeNode<Integer> tree) {
+	  
+	  int leftHeight = 0;
+	  int rightHeight = 0;
+	  
+	  if(tree == null) return 0;
+	  
+	  else {
+		  leftHeight  = treeTraversal(tree.left) + leftHeight;
+		  if(leftHeight < 0) {
+			  return -10;
+		  }
+		  rightHeight = treeTraversal(tree.right) + rightHeight;
+		  if(Math.abs(leftHeight - rightHeight) > 1) {
+			  return -10;  // just to convey that test failed along the recursive stack.
+		  }
+		  else {
+			  
+			  return leftHeight >= rightHeight ? leftHeight + 1 : rightHeight + 1;
+		  }
+	  }
+	  
   }
 
   public static void main(String[] args) {
