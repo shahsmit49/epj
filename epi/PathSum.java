345bdf99f4a396465e55e065f48ff643cb582006
Smit Shah <Smit Shah@Smit>
2019-02-06 17:00:17 +0530

    PathSum

--- epi/PathSum.java
+++ epi/PathSum.java
@@ -7,7 +7,35 @@ public class PathSum {
   public static boolean hasPathSum(BinaryTreeNode<Integer> tree,
                                    int remainingWeight) {
     // TODO - you fill in here.
-    return true;
+    return pathSums(tree, remainingWeight) == -1 ? true : false;
+  }
+  
+  public static int pathSums(BinaryTreeNode<Integer> tree,int remainingWeight) {
+	  
+	  if(tree == null) return 0;
+	  
+	  else {
+		  
+		  int data = tree.data;
+		  remainingWeight = remainingWeight - data;
+		  
+		  int leftResult = pathSums(tree.left, remainingWeight);
+		  if (leftResult == -1) {
+			return -1;
+		  }
+		  
+		  int rightResult = pathSums(tree.right, remainingWeight);
+		  if (rightResult == -1) {
+				return -1;
+		  }
+		  
+		  if(leftResult == 0 && rightResult == 0) {
+			  return remainingWeight == 0 ? -1 : -2; 
+		  }
+	  }
+	  return -2;
+	  
+	  
   }
 
   public static void main(String[] args) {
