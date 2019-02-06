4b0e711348f5d1d60bc2fc87ce2379a4e4b9b272
Smit Shah <Smit Shah@Smit>
2019-02-06 14:28:15 +0530

    SumRootToLeaf

--- epi/SumRootToLeaf.java
+++ epi/SumRootToLeaf.java
@@ -2,11 +2,38 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class SumRootToLeaf {
-  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
 
+  public static int targetSum = 0;
+  @EpiTest(testDataFile = "sum_root_to_leaf.tsv")
   public static int sumRootToLeaf(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return 0;
+    
+	inorderTrav(tree, -1);
+	int temp = targetSum;
+	targetSum = 0;
+	return temp;
+  }
+  
+  public static void inorderTrav(BinaryTreeNode<Integer> tree, int dataMSB) {
+	  
+	  if(tree == null) return;
+	  
+	  else {
+		  
+		  int data = tree.data;
+		  if(dataMSB != -1) {
+			  dataMSB = dataMSB << 1;
+			  dataMSB = dataMSB | data;
+		  }
+		  if(dataMSB == -1) {
+			  dataMSB = data;
+		  }
+		  inorderTrav(tree.left, dataMSB);
+		  
+		  inorderTrav(tree.right, dataMSB);
+		  if(tree.left == null && tree.right == null) {
+			  targetSum = targetSum + dataMSB;
+		  }
+	  }
   }
 
   public static void main(String[] args) {
