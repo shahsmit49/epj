3c0964616324240a93dec42c90b3e969e7a8bda7
Smit Shah <Smit Shah@Smit>
2019-02-13 18:31:31 +0530

    TreeInorder

--- epi/TreeInorder.java
+++ epi/TreeInorder.java
@@ -1,13 +1,42 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 public class TreeInorder {
   @EpiTest(testDataFile = "tree_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+
+	  boolean flag = true;
+	  List<Integer> answer = new ArrayList<>();
+	  Deque<BinaryTreeNode<Integer>> treeStore = new LinkedList<>();
+	  treeStore.addFirst(tree);
+	  while(tree != null) {
+		  if(tree.left != null && flag == true) {
+			  tree = tree.left;
+			  treeStore.addFirst(tree);
+		  } 
+		  else {
+			  if(tree.right != null) {
+				  answer.add(tree.data);
+				  tree = tree.right;
+				  treeStore.removeFirst();
+				  treeStore.addFirst(tree);
+				  flag = true;
+			  }
+			  else {
+				  answer.add(tree.data);
+				  tree = treeStore.removeFirst();
+				  tree = treeStore.peekFirst();
+				  flag = false;
+			  }
+		  }
+	  }
+    return answer;
   }
 
   public static void main(String[] args) {
