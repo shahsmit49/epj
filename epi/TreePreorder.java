1c1253f0033795a8d7a98c69c9d55c4a85ea25f5
Smit Shah <Smit Shah@Smit>
2019-03-05 15:00:54 +0530

    TreePreorder

--- epi/TreePreorder.java
+++ epi/TreePreorder.java
@@ -1,13 +1,28 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 public class TreePreorder {
   @EpiTest(testDataFile = "tree_preorder.tsv")
 
   public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    
+	Deque<BinaryTreeNode<Integer>> treeStack = new LinkedList<>();
+	List<Integer> answer = new ArrayList<>();
+	treeStack.addFirst(tree);
+	while(!treeStack.isEmpty()) {
+		tree = treeStack.pollFirst();
+		if(tree != null) {
+			answer.add(tree.data);
+			treeStack.addFirst(tree.right);
+			treeStack.addFirst(tree.left);
+		}
+	}
+    return answer;
   }
 
   public static void main(String[] args) {
